13705b8f34b59dfe21c12e1e0a1c9910675d71f5
tlacuilose <tlacuilose@outlook.com>
2024-03-18 02:16:35 -0600

    waiting for fix to be in repo

--- lua/myconfig/remap.lua
+++ lua/myconfig/remap.lua
@@ -1,3 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader>rr", ":bufdo e<CR>")
+vim.keymap.set("n", "<leader>cc", ":CopilotChatToggle<CR>")
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
