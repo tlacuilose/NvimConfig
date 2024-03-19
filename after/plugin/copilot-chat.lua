dec16d238d3048762f5576e92d8ac1e4f2fcd652
tlacuilose <tlacuilose@outlook.com>
2024-03-19 06:53:47 -0600

    Fix merged in plugins repo

--- after/plugin/copilot-chat.lua
+++ after/plugin/copilot-chat.lua
@@ -2,9 +2,11 @@ require('CopilotChat').setup {
     debug = true,
 }
 
+--[[
 vim.api.nvim_create_autocmd('BufEnter', {
     pattern = 'copilot-*',
     callback = function()
-        vim.opt_local.relativenumber = true
+        vim.opt_local.relativenumber = false
     end
 })
+]]
